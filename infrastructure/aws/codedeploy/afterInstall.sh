dc9708737b7927923651fa7695967dc6ecb0c95a
Rahul Zore <zore.r@husky.neu.edu>
2019-03-06 07:06:19 -0500

    21th commit for assignment5

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl stop tomcat.service
 
-#sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
+sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
 
 sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 
