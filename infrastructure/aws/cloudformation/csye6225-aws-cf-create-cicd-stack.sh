4a6f3b1aa4618997904adbe5af76bd3b55e784e9
Nishad <ranadive.n@husky.neu.edu>
2019-03-13 00:18:14 -0400

    files added 10

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -17,9 +17,11 @@ DOMAINNAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output 
 DNS=${DOMAINNAME::-1}
 echo $DNS
 
-CODEDEPLOYS3BUCKETNAME="code-deploy.${DNS}"
+CODEDEPLOYS3BUCKETNAME="code-deploy-${DNS}"
 echo $CODEDEPLOYS3BUCKETNAME
-aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=CodeDeployEC2ServiceRoleName,ParameterValue=$CODEDEPLOYEC2SERVICEROLENAME ParameterKey=CircleCiUploadtoS3PolicyName,ParameterValue=$CIRCLECIUPLOADTOS3POLICYNAME ParameterKey=CircleCiUser,ParameterValue=$CIRCLECIUSER ParameterKey=CodeDeployS3BucketName,ParameterValue=$CODEDEPLOYS3BUCKETNAME ParameterKey=CodeDeployApplicationName,ParameterValue=$CODEDEPLOYAPPNAME ParameterKey=AWSRegion,ParameterValue=$AWSREGION ParameterKey=AWSAccountID,ParameterValue=$AWSACCOUNTID ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODEDEPLOYSERVICEROLENAME ParameterKey=CodeDeployEC2S3PolicyName,ParameterValue=$CODEDEPLOYEC2S3POLICYNAME ParameterKey=CircleCiCodeDeployPolicyName,ParameterValue=$CIRCLECICODEDEPLOYPOLICYNAME ParameterKey=CircleCiEC2AMIPolicyName,ParameterValue=$CIRCLECIEC2AMIPOLICYNAME
+BUCKETNAME="${DNS}.csye6225.com"
+echo $BUCKETNAME
+aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=CodeDeployEC2ServiceRoleName,ParameterValue=$CODEDEPLOYEC2SERVICEROLENAME ParameterKey=CircleCiUploadtoS3PolicyName,ParameterValue=$CIRCLECIUPLOADTOS3POLICYNAME ParameterKey=CircleCiUser,ParameterValue=$CIRCLECIUSER ParameterKey=CodeDeployS3BucketName,ParameterValue=$CODEDEPLOYS3BUCKETNAME ParameterKey=CodeDeployApplicationName,ParameterValue=$CODEDEPLOYAPPNAME ParameterKey=AWSRegion,ParameterValue=$AWSREGION ParameterKey=AWSAccountID,ParameterValue=$AWSACCOUNTID ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODEDEPLOYSERVICEROLENAME ParameterKey=CodeDeployEC2S3PolicyName,ParameterValue=$CODEDEPLOYEC2S3POLICYNAME ParameterKey=CircleCiCodeDeployPolicyName,ParameterValue=$CIRCLECICODEDEPLOYPOLICYNAME ParameterKey=CircleCiEC2AMIPolicyName,ParameterValue=$CIRCLECIEC2AMIPOLICYNAME ParameterKey=bucketname,ParameterValue=$BUCKETNAME 
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
 
