1b403711879fd7ca1ae24996c96c8088b725b448
Rahul Zore <zore.r@husky.neu.edu>
2019-03-20 19:19:05 -0400

    Changes to webapp startup files

--- webapp/src/main/java/com/cloud/webapp/ServletInitializer.java
+++ webapp/src/main/java/com/cloud/webapp/ServletInitializer.java
@@ -3,10 +3,10 @@ package com.cloud.webapp;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebappApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(WebappApplication.class);
+//	}
 }
--- webapp/src/main/java/com/cloud/webapp/WebappApplication.java
+++ webapp/src/main/java/com/cloud/webapp/WebappApplication.java
@@ -2,10 +2,17 @@ package com.cloud.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebappApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
