19d916c75e9de7bbc15e06dc2e156ddc98664921
Rahul Zore <zore.r@husky.neu.edu>
2019-03-19 23:46:34 -0400

    Added configuration to webapp for custom metrics collection and code cleanup

--- webapp/src/main/java/com/cloud/webapp/config/CloudwatchMetricsConfig.java
+++ webapp/src/main/java/com/cloud/webapp/config/CloudwatchMetricsConfig.java
@@ -3,14 +3,12 @@ package com.cloud.webapp.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.timgroup.statsd.NoOpStatsDClient;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
 
 @Configuration
-@Profile("prod")
 public class CloudwatchMetricsConfig {
 
 	@Value("${metrics.publish}")
